71f42ec42da3c3f958419e7bb6ba2d6ebbae50ce
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-05-19 15:56:40 +0200

    Refactorizando CreateOrderDto y añadiendo OrderItemDto

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,29 +1,11 @@
-import {
-  IsBoolean,
-  IsEnum,
-  IsNumber,
-  IsOptional,
-  IsPositive,
-} from 'class-validator';
-import { OrderStatus } from '../../../generated/prisma/enums';
-import { OrderStatusList } from '../enum/order.enum';
+import { ArrayMinSize, IsArray, ValidateNested } from 'class-validator';
+import { OrderItemDto } from '.';
+import { Type } from 'class-transformer';
 
 export class CreateOrderDto {
-  @IsNumber()
-  @IsPositive()
-  totalAmount!: number;
-
-  @IsNumber()
-  @IsPositive()
-  totalItems!: number;
-
-  @IsEnum(OrderStatusList, {
-    message: `Possible status values are ${OrderStatusList}`,
-  })
-  @IsOptional()
-  status: OrderStatus = OrderStatus.PENDING;
-
-  @IsBoolean()
-  @IsOptional()
-  paid: boolean = false;
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items!: OrderItemDto[];
 }
